f1f882f528edca5ac4c480bf280609a568781a15
Alexander Urizar <urizaraj@gmail.com>
2018-02-07 22:28:14 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != ' '
+  space = board[index]
+  if space == 'X' || space == "O"
     return true
   else
     return false
